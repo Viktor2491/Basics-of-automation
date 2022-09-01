aa524daa00e9f7a6dd50bbac6952a7a82c95f496
viktor91 <v.a.taradaiko@gmail.com>
2022-09-01 10:54:37 +0400

    Completing the second part of the task

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void purchaseLessThanThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int boundary = 800;
+        int expected = 200;
+        int actual = service.remain(boundary);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void purchaseMoreThanThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int boundary = 1700;
+        int expected = 300;
+        int actual = service.remain(boundary);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void purchaseEqualToThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int boundary = 1000;
+        int expected = 0;
+        int actual = service.remain(boundary);
+        Assert.assertEquals(actual, expected);
+    }
+}
